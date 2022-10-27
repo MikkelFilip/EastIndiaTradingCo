18cd3f66f6f7682561b6eadea279c55241ab6094
ncdmz\bmtr <bmtr@netcompany.com>
2022-10-27 21:11:18 +0700

    configuration for functional endpoint

--- EITBackend/EITBackend/External/ExternalController.cs
+++ EITBackend/EITBackend/External/ExternalController.cs
@@ -1,23 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EITBackend.Common.DTOs;
+using EITBackend.Common.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EITBackend.External
 {
     [ApiController]
     [Route("/api/external/EIT/[controller]")]
-    public class ExternalController : ControllerBase
+    public class ExternalController : Controller
     {
 
-        private readonly ILogger<ExternalController> _logger;
+        private readonly ILogger<ExternalController> logger;
+        private readonly IConnectedCitiesService connectedCitiesService;
 
-        public ExternalController(ILogger<ExternalController> logger)
+        public ExternalController(IConnectedCitiesService connectedCitiesService, ILogger<ExternalController> logger)
         {
-            _logger = logger;
+            this.connectedCitiesService = connectedCitiesService;
+            this.logger = logger;
         }
 
         [HttpGet(Name = "GetConnectedCities")]
-        public string GetConnectedCities()
+        public GetConnectedCities GetConnectedCitiesEndpoint([FromQuery(Name = "cityName")] string cityName, [FromQuery(Name = "weight")] int weight, [FromQuery(Name = "contentType")] string contentType, [FromQuery(Name = "dateTime")] DateTime dateTime, [FromQuery(Name = "packageType")] string packageType)
         {
-            return "it works";
+            return connectedCitiesService.GetConnectedCities(cityName, weight, contentType, dateTime, packageType);
         }
     }
 }
--- EITBackend/EITBackend/Program.cs
+++ EITBackend/EITBackend/Program.cs
@@ -1,3 +1,8 @@
+using EITBackend.Common.Adapters;
+using EITBackend.Common.Adapters.IAdapters;
+using EITBackend.Common.Services;
+using EITBackend.Common.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IConnectedCitiesService, ConnectedCitiesService>();
+builder.Services.AddScoped<IConnectedCitiesSegmentAdapter, ConnectedCitiesSegmentAdapter>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
