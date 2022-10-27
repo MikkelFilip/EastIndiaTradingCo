330d42d1bcbae47f98c9df45e76bfa7e16994575
Nguyễn Thành Vương <ntvu@netcompany.com>
2022-10-27 20:57:40 +0200

    Add Dtos PossibleRoute and QueryPossibleRoute

---
+++ EITBackend/EITBackend/Common/DTOs/ContentType.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EITBackend.Common.DTOs
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ContentType
+    {
+        RecordedDelivery,
+        Weapons,
+        LiveAnimals,
+        CautiousParcels,
+        RefrigeratedGoods,
+        Other
+    }
+}
--- EITBackend/EITBackend/Common/DTOs/QueryPossibleRoute.cs
+++ EITBackend/EITBackend/Common/DTOs/QueryPossibleRoute.cs
@@ -1,7 +1,26 @@
-﻿namespace EITBackend.Common.DTOs
+﻿using EITBackend.Common.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EITBackend.Common.DTOs
 {
     public class QueryPossibleRoute
     {
+        [Required]
+        public City From { get; set; } = null!;
 
+        [Required]
+        public City To { get; set; } = null!;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public ContentType ContentType { get; set; }
+
+        [Required]
+        public PackageType PackageType { get; set; }
+
+        [Required]
+        public double Weight { get; set; } = 0;
     }
 }
