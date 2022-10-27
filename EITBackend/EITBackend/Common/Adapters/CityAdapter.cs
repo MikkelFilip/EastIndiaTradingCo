7fd162641b30ad84ddfa7a8f6569996c0f8794dd
ncdmz\bmtr <bmtr@netcompany.com>
2022-10-27 23:16:10 +0700

    add city adapter and new functions for cities

---
+++ EITBackend/EITBackend/Common/Adapters/CityAdapter.cs
@@ -0,0 +1,23 @@
+﻿using EITBackend.Common.Adapters.IAdapters;
+using EITBackend.Common.Models;
+
+namespace EITBackend.Common.Adapters
+{
+    public class CityAdapter : ICityAdapter
+    {
+        private DataContext context;
+        public CityAdapter(DataContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<City> GetAllCity()
+        {
+            return context.cities.ToList();
+        }
+
+        public string GetCityName(int cityId)
+        {
+            return context.cities.Where(city => city.CityId == cityId).Select(city => city.Name).FirstOrDefault();
+        }
+    }
+}
