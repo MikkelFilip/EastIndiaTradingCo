f3f48dce480b0ea061d265c9149bd7f72c46b1ba
malenehelsem <helsem.malene@gmail.com>
2022-10-27 19:44:46 +0200

    Add external endpoint for GetContentTypes

--- EITBackend/EITBackend/External/ExternalController.cs
+++ EITBackend/EITBackend/External/ExternalController.cs
@@ -6,23 +6,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace EITBackend.External
 {
     [ApiController]
-    [Route("/api/GetConnectedCities")]
+    [Route("/api")]
     public class ExternalController : Controller
     {
 
         private readonly ILogger<ExternalController> logger;
         private readonly IConnectedCitiesService connectedCitiesService;
+        private DataContext context;
 
-        public ExternalController(IConnectedCitiesService connectedCitiesService, ILogger<ExternalController> logger)
+        public ExternalController(IConnectedCitiesService connectedCitiesService, ILogger<ExternalController> logger, DataContext context)
         {
             this.connectedCitiesService = connectedCitiesService;
             this.logger = logger;
+            this.context = context;
         }
 
-        [HttpGet(Name = "{cityName}")]
+        [HttpGet("GetConnectedCities/{cityName}")]
         public GetConnectedCities GetConnectedCitiesEndpoint(string cityName, [FromQuery(Name = "weight")] int weight, [FromQuery(Name = "contentType")] string contentType, [FromQuery(Name = "dateTime")] DateTime dateTime, [FromQuery(Name = "packageType")] string packageType)
         {
             return connectedCitiesService.GetConnectedCities(cityName, weight, contentType, dateTime, packageType);
+        }
+
+        [HttpGet("GetContentTypes")]
+        public IEnumerable<ContentType> GetContentTypeEndpoint()
+        {
+            return context.contentTypes.ToList();
         }
     }
 }
